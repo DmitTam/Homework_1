f6a4493b8b1d654d7ce227ef04a51f6a21eb0494
TamaraDmitrieva <alexidy@mail.ru>
2023-09-24 22:00:48 +0300

    Exercise8 finish

---
+++ Exercise8/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает 
+// все чётные числа от 1 до N.
+// 5 -> 2, 4
+
+Console.WriteLine("Введите целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+if (number > 1)
+{
+    while (count < number)
+    {
+        int A = count % 2;
+        if (count+2 <= number)
+        if (A == 0)
+        {
+            Console.Write($"{count+2} ");
+        };
+        count++;
+    }
+};
