4ecf593a0a352641959b601bc25e8a838cc4d340
TamaraDmitrieva <alexidy@mail.ru>
2023-09-27 23:21:38 +0300

    Exercise 15 in work

---
+++ Exercise13/Program.cs
@@ -0,0 +1,36 @@
+﻿// /Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+
+int ThirdDigit(int num)
+{
+    // int secondDigit = num / 10 % 10;
+    return num % 10;
+    // Console.WriteLine($"-> {secondDigit}");
+}
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number <100) 
+{
+    Console.WriteLine("-> третьей цифры нет");
+    return;
+}
+
+if (number >1000)
+{
+    
+}
+int a = ThirdDigit(number);
+Console.WriteLine($"-> {a}");
+
+
+
+
+
+
+
+
---
+++ Exercise15/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введите порядковый номер дня недели");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 5 || number > 0) 
+{
+    Console.WriteLine(" -> нет");
+    return;
+}
+if (number == 6) Console.WriteLine(" -> да");
+if (number == 7) Console.WriteLine(" -> да");
+
+bool WeekDay(int num)
+{
+    return 
+}
