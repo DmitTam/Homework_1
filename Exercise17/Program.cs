f88cac7f3735618070ab0e188953d14a6cd6c21d
TamaraDmitrieva <alexidy@mail.ru>
2023-09-28 21:46:45 +0300

    Exercises 17,24

---
+++ Exercise17/Program.cs
@@ -0,0 +1,39 @@
+﻿
+
+
+
+
+
+public class Answer
+{
+    static bool IsPalindrome(int number)
+    {
+        int firstDigit = number / 10000;
+        int secondDigit = number / 1000 % 10;
+        int forthDigit = number % 100;
+        int fifthDigit = number % 10;
+        if (firstDigit == fifthDigit || secondDigit == fifthDigit)
+            { return true; }
+            return false;
+            if (number > 99999 | number < 10000)
+            {
+                Console.WriteLine("число не пятизначное");
+            }
+    }
+
+    static public void Main(string[] args) {
+        int number;
+
+        if (args.Length >= 1)
+        {
+            number = int.Parse(args[0]);
+        }
+        else
+        {
+            number = 99;
+        }
+        bool result = IsPalindrome(number);
+        System.Console.WriteLine($"{result}");
+    }
+}
+
